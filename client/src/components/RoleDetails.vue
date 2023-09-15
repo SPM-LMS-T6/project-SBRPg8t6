b0e6a2fdfa7c0ed6f1ed4aee59befb3fa82da2a3
rayquekstoic <132422548+rayquekstoic@users.noreply.github.com>
2023-09-15 16:52:51 +0800

    A2SG-35 feat: add RoleDetails component

---
+++ client/src/components/RoleDetails.vue
@@ -0,0 +1,12 @@
+<script setup>
+const { roleDetails } = defineProps({
+  roleDetails: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div>{{ roleDetails }}</div>
+</template>
