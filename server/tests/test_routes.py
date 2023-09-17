efd567c1bcfbcfbda948454270d62877af0f0d8e
a2ronk <aaronkwah@gmail.com>
2023-09-17 12:43:04 +0800

    A2SG-17 fix: test workflow

---
+++ server/tests/test_routes.py
@@ -0,0 +1,14 @@
+"""
+Test the routes of the server
+"""
+
+from server.server import app
+
+
+def test_home():
+    """
+    Test the home route
+    """
+    client = app.test_client()
+    response = client.get("/")
+    assert b"Hello World!" in response.data
