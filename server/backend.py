fc9dcdf10fb58138a79a85e1f546457c5dfcd52d
a2ronk <aaronkwah@gmail.com>
2023-09-17 12:38:03 +0800

    A2SG-17 feat: add create role listing endpoint

--- server/backend.py
+++ server/backend.py
@@ -7,84 +7,100 @@ import datetime, json
 app = Flask(__name__)
 CORS(app)
 
+
 ##Get Connection
 def get_db_connection():
-    return mysql.connector.connect(user='eduhub_user', host='eduhub_db_mysql', port='3306', password='eduhub_password', database='eduhub_db_mysql')
-    
+    return mysql.connector.connect(
+        user="eduhub_user",
+        host="eduhub_db_mysql",
+        port="3306",
+        password="eduhub_password",
+        database="eduhub_db_mysql",
+    )
 
 
-@app.route("/create", methods=['POST'])
+# Create rolelisting variable to be added infront of the app.route
+rolelisting = "/rolelistings"
+
+# Create version variable to be added infront of the app.route
+v1 = "/v1"
+
+
+# Role Listings
+@app.route(f"{v1}{rolelisting}/create", methods=["POST"])
 def create_role():
-
-    ##Input Validation
-    err_msg = []
-    if request.form['role_listing_id'].isnumeric:
-        role_listing_id = request.form['role_listing_id']
-    else:
-        err_msg.append("Input Correct ID")        
-    if request.form['role_listing_creator'].isnumeric:
-        role_creator = request.form['role_listing_creator']
-    else:
-        err_msg.append("Input Correct Creator")
-    if isinstance(request.form['role_listing_source'], str) and len(request.form['role_listing_source']) < 50:
-        role_source = request.form['role_listing_source']
-    else:
-        err_msg.append("Input Correct Source")
-    if request.form['role_listing_updater'].isnumeric:
-        role_updater = request.form['role_listing_updater']
-    else:
-        err_msg.append("Input Correct Updater")
-    if request.form['role_id'].isnumeric:
-        role_id = request.form['role_id']
-    else:
-        err_msg.append("Input Correct ID")
-    if isinstance(request.form['role_listing_desc'], str):
-        role_desc = request.form['role_listing_desc']
-    else:
-        err_msg.append("Input Correct description")
-    if datetime.date.fromisoformat(request.form['role_listing_open']):
-        open = request.form['role_listing_open']
-    else:
-        err_msg.append("Input Correct Date")
-    if datetime.date.fromisoformat(request.form['role_listing_close']):
-        close = request.form['role_listing_open']
-    else:
-        err_msg.append("Input Correct Date")
-
-
-    if err_msg != []:
-        return print(err_msg[0])
-    else:
-        ##Create Record for Role Listing
-        conn = get_db_connection()
+    if request.is_json:
+        print("\nReceived data in JSON format")
         try:
-            cur = conn.cursor()
-            cur.execute('INSERT INTO role_listings(role_listing_id, role_listing_creator,role_listing_source,role_listing_updater, role_id, role_listing_desc, role_listing_open, role_listing_close)'
-                        'VALUES (%s, %s, %s, %s, %s, %s, %s, %s)',
-                        (role_listing_id, role_creator,role_source,role_updater,role_id, role_desc, open, close))
-            conn.commit()
-        except Exception as e:
-            print("Error in SQL:\n", e)
-        finally:
-            conn.close()
-        return "Created Role"
+            data = request.get_json()
+            role_listing_id = data["role_listing_id"]
+            role_listing_creator = data["role_listing_creator"]
+            role_listing_source = data["role_listing_source"]
+            role_listing_updater = data["role_listing_updater"]
+            role_id = data["role_id"]
+            role_listing_desc = data["role_listing_desc"]
+            role_listing_open = data["role_listing_open"]
+            role_listing_close = data["role_listing_close"]
 
-@app.route("/get", methods=['GET'])
-def get_role():
-     ##Get Record for Role Listing
-    conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute('SELECT * from staff_details')
-    data = cur.fetchall()
-    cur.close()
-    conn.close()
-    # Check if the data list is empty
-    if not data:
-        return jsonify(["Empty records for staff"])
-    json_data = [record.json() for record in data]
-    return jsonify(json_data)
-    
-    
+            ## Validate Data
+            err_msg = []
+            if not isinstance(role_listing_id, int):
+                err_msg.append("Input Correct ID")
+            if not isinstance(role_listing_creator, int):
+                err_msg.append("Input Correct Creator")
+            if (
+                not isinstance(role_listing_source, str)
+                and len(role_listing_source) < 50
+            ):
+                err_msg.append("Input Correct Source")
+            if not isinstance(role_listing_updater, int):
+                err_msg.append("Input Correct Updater")
+            if not isinstance(role_id, int):
+                err_msg.append("Input Correct ID")
+            if not isinstance(role_listing_desc, str):
+                err_msg.append("Input Correct description")
+            if not isinstance(role_listing_open, str):
+                err_msg.append("Input Correct Date")
+            if not isinstance(role_listing_close, str):
+                err_msg.append("Input Correct Date")
+            if err_msg != []:
+                return print(err_msg[0])
+            else:
+                print("\n Clear Validation")
+                ##Create Record for Role Listing
+                conn = get_db_connection()
+
+                try:
+                    cur = conn.cursor()
+                    cur.execute(
+                        "INSERT INTO role_listings(role_listing_id, role_listing_creator,role_listing_source,role_listing_updater, role_id, role_listing_desc, role_listing_open, role_listing_close)"
+                        "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
+                        (
+                            role_listing_id,
+                            role_listing_creator,
+                            role_listing_source,
+                            role_listing_updater,
+                            role_id,
+                            role_listing_desc,
+                            role_listing_open,
+                            role_listing_close,
+                        ),
+                    )
+                    conn.commit()
+                    # Return json code 201 for created
+                    return jsonify({"code": 201, "message": "Created Role"})
+                except Exception as e:
+                    # Return json code 400 for bad request
+                    return jsonify({"code": 400, "message": "Bad Request"})
+                finally:
+                    conn.close()
+        except Exception as e:
+            # Return json code 400 for bad request
+            return jsonify({"code": 400, "message": err_msg})
+
+    else:
+        return jsonify({"code": 400, "message": "Request not in JSON format"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5101, debug=True)
