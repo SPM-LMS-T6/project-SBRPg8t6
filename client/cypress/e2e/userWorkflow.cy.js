2a290d6ed7b17337c7b576e5e3ec6d17fa05412b
BobzBanana <xavier.koo.2021@scis.smu.edu.sg>
2023-10-23 01:26:12 +0800

    A2SG-86 feat: finalized user workflow tests

--- client/cypress/e2e/userWorkflow.cy.js
+++ client/cypress/e2e/userWorkflow.cy.js
@@ -3,10 +3,23 @@ describe('User Workflow', () => {
     cy.visit('http://localhost:8080')
   })
 
-  //TODO: add Login tests
+  it('should display the login page', () => {
+    cy.get('.login').should('exist')
+  })
 
-  //TODO: update any new UI Elements
-  it('should display role listings on the page', () => {
+  it('should display all relevant elements in the login page', () => {
+    cy.get('.login').should('exist')
+    cy.get('.talent').should('exist')
+    cy.get('.title').should('exist').contains('Discover The Perfect Role:')
+    cy.get('.followup')
+      .should('exist')
+      .contains('Finding the Ideal Fit for Your Talents and Aspirations')
+    cy.get('.form-group').should('exist')
+    cy.get('.defaultBtn').should('exist').contains('Log In')
+  })
+
+  it('should log in as an employee and redirect to the role listings page', () => {
+    cy.get('.defaultBtn').click()
     // Check if the container with class 'container-fluid' exists
     cy.get('.container-fluid').should('exist')
 
@@ -15,12 +28,14 @@ describe('User Workflow', () => {
   })
 
   it('should filter roles by skill successfully', () => {
+    cy.get('.defaultBtn').click()
     // Assuming there's a dropdown with class 'form-select' and options available
     cy.get('select.form-select').select('Python Programming')
     cy.get('.job-role-item').should('have.length.greaterThan', 0)
   })
 
   it('should display all relevant details in a job role item', () => {
+    cy.get('.defaultBtn').click()
     // Assuming there's at least one job role item
     cy.get('.job-role-item').first().should('exist')
     cy.get('.card-title').should('exist') // role name
@@ -31,21 +46,21 @@ describe('User Workflow', () => {
   })
 
   it('should display expanded role details when a role header is clicked', () => {
+    cy.get('.defaultBtn').click()
     // Assuming there's at least one job role item
     cy.get('.job-role-item').first().click()
     cy.get('.card-text').should('exist')
   })
 
   it('should display all relevant elements in the expanded role details', () => {
+    cy.get('.defaultBtn').click()
     // Assuming there's at least one job role item
     cy.get('.job-role-item').first().click()
     cy.get('.roleDetails').should('exist')
     cy.get('.my-auto').should('exist') // role name
-    cy.get('.updateBtn').should('exist') // update button
     cy.get('.fw-bold').should('exist').contains('Posted On:') // posted on label
     cy.get('.check').should('exist') // posted on date and deadline
     cy.get('.fw-bold.isPosted').should('exist') // posted by or updated by label
-    cy.get('.check.isCreated').should('exist') // posted by or updated by name
     cy.get('.fw-bold').should('exist').contains('Deadline:') // deadline label
     cy.get('.skills-container').should('exist') // skills container
     cy.get('.artdeco-button__text').should('exist').contains('Apply') // apply button
@@ -53,12 +68,14 @@ describe('User Workflow', () => {
   })
 
   it('should open the application modal when apply button is clicked', () => {
+    cy.get('.defaultBtn').click()
     cy.get('.job-role-item').first().click()
     cy.get('button[data-bs-target="#applicationModal"]').click()
     cy.get('#applicationModal').should('exist')
   })
 
   it('should display all relevant elements in the application modal', () => {
+    cy.get('.defaultBtn').click()
     cy.get('.job-role-item').first().click()
     cy.get('button[data-bs-target="#applicationModal"]').click()
     cy.get('#application')
@@ -71,32 +88,39 @@ describe('User Workflow', () => {
   })
 
   it('should close the application modal when back button is clicked', () => {
+    cy.get('.defaultBtn').click()
     cy.get('.job-role-item').first().click()
     cy.get('button[data-bs-target="#applicationModal"]').click()
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
     cy.wait(500) // wait for modal to open
     cy.get('button[aria-label="Close"]').click()
     cy.get('label[roleName]').should('not.exist')
   })
 
-  it('should be able to submit an application', () => {
+  it('should not be able to submit an application for an applied role', () => {
+    cy.get('.defaultBtn').click()
     cy.get('.job-role-item').first().click()
     cy.get('button[data-bs-target="#applicationModal"]').click()
     cy.get('#answer').type('Hire Me!')
     cy.get('.modal-footer').contains('Submit').click()
-    cy.get('h3').contains('Application Successfully Submitted')
+    cy.get('.alert').contains('You have already applied for this role')
   })
 
   it('should be able to navigate to the application status page', () => {
+    cy.get('.defaultBtn').click()
     cy.get('.nav-link').contains('Application Status').click()
   })
 
   it('should display all relevant elements in the application status page', () => {
+    cy.get('.defaultBtn').click()
     cy.get('.nav-link').contains('Application Status').click()
     cy.get('.table').should('exist')
   })
 
-  it('should contain the application made in the application modal', () => {
+  it('should contain the applied application', () => {
+    cy.get('.defaultBtn').click()
     cy.get('.nav-link').contains('Application Status').click()
-    cy.get('.table').contains('Hire Me!')
+    cy.get('.table').should('exist').contains('Agile Coach (SM)')
+    cy.get('.table').contains('applied')
   })
 })
