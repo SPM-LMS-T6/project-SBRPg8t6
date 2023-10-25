20d08ec362c3e5125636ee687795c8e64bb68e96
BobzBanana <xavier.koo.2021@scis.smu.edu.sg>
2023-10-25 17:09:15 +0800

    A2SG-85 feat: add hr workflow tests

--- client/cypress/e2e/hrWorkflow.cy.js
+++ client/cypress/e2e/hrWorkflow.cy.js
@@ -59,6 +59,26 @@ describe('HR Workflow', () => {
     cy.get('.description').should('exist') // description
   })
 
+  it('should open the application modal when apply button is clicked', () => {
+    cy.get('.defaultBtn').click()
+    cy.get('.job-role-item').first().click()
+    cy.get('.badge.rounded-pill.bg-primary.text-white.p-2').first().click()
+    cy.get('#applicationModal').should('exist')
+  })
+
+  it('should display all relevant elements in the application modal', () => {
+    cy.get('.defaultBtn').click()
+    cy.get('.job-role-item').first().click()
+    cy.get('.badge.rounded-pill.bg-primary.text-white.p-2').first().click()
+    cy.get('#application')
+      .should('exist')
+      .contains(/Role Listing ID: \d+/) // role listing id dynamic id
+    cy.get('#roleName').should('have.value', 'Agile Coach (SM)')
+    cy.get('.form-label').should('exist').contains('Why do you want this role? (Max 100 words)')
+    cy.get('.form-control').should('exist')
+    cy.get('.modal-footer').should('exist').contains('Submit')
+  })
+
   it('should display update role listing page when update button is clicked', () => {
     cy.get('#dropdown').select('HR')
     cy.get('.defaultBtn').click()
@@ -76,16 +96,58 @@ describe('HR Workflow', () => {
     cy.get('.job-role-item').should('exist')
   })
 
-  it('should return an error message when update button is clicked with close date that is not later than or equal to the current date', () => {
+  it('should return two error messages when update button is clicked with close date that is not later than or equal to the current date and Role Listing Open date not earlier than Role Listing Close date', () => {
     cy.get('#dropdown').select('HR')
     cy.get('.defaultBtn').click()
     cy.get('.job-role-item').first().click()
     cy.get('.artdeco-button__text').contains(' Update ').click()
+    cy.get('#startDate').type('2020-01-01')
+    cy.get('#closeDate').type('2020-01-01')
     cy.get('#update').should('exist').click()
     cy.get('.my-auto')
       .should('exist')
       .contains('Role Listing Close date must be a date later than or equal to the Current date')
+    cy.get('.my-auto').contains(
+      'Role Listing Open date must be a date earlier than Role Listing Close date'
+    )
+  })
+  it('should return a success message when role listing is updated successfully', () => {
+    cy.get('#dropdown').select('HR')
+    cy.get('.defaultBtn').click()
+    cy.get('.job-role-item').first().click()
+    cy.get('.artdeco-button__text').contains(' Update ').click()
+    cy.get('#startDate').type('2030-01-01')
+    cy.get('#closeDate').type('2050-01-01')
+    cy.get('#update').should('exist').click()
+    cy.get('.noti').should('exist').contains('Role Listing Successfully Updated')
   })
 
-  // TODO continue test date mismatch error
+  it('should navigate to the create role listing page when create role listing nav bar button is clicked', () => {
+    cy.get('#dropdown').select('HR')
+    cy.get('.defaultBtn').click()
+    cy.get('.create').click()
+    cy.get('#exampleModal').should('exist')
+  })
+
+  it('should show error notification when create role listing button is clicked with invalid input', () => {
+    cy.get('#dropdown').select('HR')
+    cy.get('.defaultBtn').click()
+    cy.get('.create').click()
+    cy.get('#create').click()
+    cy.get('.text-danger').should('exist').contains('Role Listing Creation Failed')
+  })
+
+  it('should direct to view role applicants page when view role applicants button is clicked', () => {
+    cy.get('#dropdown').select('HR')
+    cy.get('.defaultBtn').click()
+    cy.get('.active.applicants').click()
+    cy.get('h1').should('exist').contains('Role Applicants')
+  })
+
+  it('should logout when logout button is clicked', () => {
+    cy.get('#dropdown').select('HR')
+    cy.get('.defaultBtn').click()
+    cy.get('.nav-link.logout').first().click()
+    cy.get('.login').should('exist')
+  })
 })
